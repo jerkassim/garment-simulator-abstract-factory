0aba645df6e6b5adcc971aa76c91cf0309e4f33b
jk <132644685+jerkassim@users.noreply.github.com>
2026-04-01 04:05:34 -0400

    Implement CasualFactory for garment creation

---
+++ CasualFactory.java
@@ -0,0 +1,13 @@
+public class CasualFactory implements GarmentFactory {
+    public Top createTop() {
+        return new CasualTop();
+    }
+
+    public Pants createPants() {
+        return new CasualPants();
+    }
+
+    public Shoes createShoes() {
+        return new CasualShoes();
+    }
+}
